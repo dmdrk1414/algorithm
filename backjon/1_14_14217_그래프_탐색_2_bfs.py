d3b598531e8dea91c4774df013abf26613c62f41
박승찬 <seungchan141414@gmail.com>
2024-01-14 18:53:44 +0900

    1_14_14217_그래프_탐색_2_bfs

---
+++ backjon/1_14_14217_그래프_탐색_2_bfs.py
@@ -0,0 +1,61 @@
+# https://www.acmicpc.net/problem/14217
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+L = [[0] * (n + 1) for _ in range(n + 1)]
+INF = 9999;
+distance = [INF for _ in range(n + 1)]
+distance[1] = 0
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    L[a][b] = 1
+    L[b][a] = 1
+
+q = int(input())
+
+def bfs(start):
+    visited = [False for _ in range(n+1)]
+    que = deque([start])
+    visited[start[0]] = True
+
+    while que:
+        value, path = que.popleft()
+        if(distance[value] > path):
+            distance[value] = path
+        for idx in range(len(L[value])):
+            if not visited[idx] and L[value][idx] == 1:
+                visited[idx] = True
+                que.append([idx, distance[value] + 1])
+
+for _ in range(q):
+    a, b = map(int, input().split())
+    L[a][b] = 1;
+    L[b][a] = 1;
+
+    bfs([1, 0])
+
+    for idx in range(1, n+1):
+        if (distance[idx] == INF):
+            print(-1, end=" ")
+        else:
+            print(distance[idx], end=" ")
+    print()
+
+"""
+5 3
+1 2
+1 3
+2 4
+5
+2 3
+4 5
+3 4
+1 4
+1 5
+
+
+"""
--- pratice copy.py
+++ pratice copy.py
@@ -2,63 +2,50 @@ import sys
 from collections import deque
 
 input = sys.stdin.readline
+INF = 9999;
 
+# 첫째 줄에는 도시의 개수 n,도로의 개수 m이 주어진다.
 n, m = map(int, input().split())
-INF = 1001
-# L은 길이다.
 L = [[0] * (n + 1) for _ in range(n + 1)]
 
-for _ in range(m):
+# 다음 m개의 줄에는 두 도시가 주어진다.(2≤n≤500,1≤m≤n*(n-1)/2)
+for _ in range (m):
     a, b = map(int, input().split())
     L[a][b] = 1
     L[b][a] = 1
-t = int(input())
 
-# 1부터 시작해서 각 value까지 BFS를 도는데,
-# path에 0부터 시작해서 다음 value를 호출할 때마다 1씩 더한값을 queue에 넣는다.
-# 이때 path와 distance array를 비교하면서 더 작은 값(짧은 거리)으로 distance array를 갱신시킨다.
+q = int(input())
 
-# line 19 ~ 20 : 이때 매 테스트 케이스마다 최단거리가 바뀔 수 있기 때문에 (도로가 사라진 경우),
-# 도로가 항상 추가되어 최단거리가 항상 짧아지는 경우만 있었던
-# 이전 문제와 다르게 distance array를 초기화 시켜주는 작업이 필요했다.
-# * 이때 전역변수 처리를 주의하자
-
-
-def bfs(target):
-    distance = [INF for _ in range(n + 1)]
-    distance[1] = 0
+def bfs(start):
+    que = deque([start])
     visited = [False for _ in range(n + 1)]
+    distance = [INF for _ in range(n + 1)]
+    visited[start[0]] = True
+    distance[start[0]] = start[1]
 
-    queue = deque()
-    queue.append(target)
-    while (queue):
-
-        value, path = queue.popleft()  # 1, 0
-        if (distance[value] > path):
+    while que:
+        value, path = que.popleft()
+        if(distance[value] > path):
             distance[value] = path
-        visited[value] = True
-
-        for i in range(len(L[value])):
-            if (L[value][i] == 1 and visited[i] == False):
-                queue.append([i, path + 1])
-                visited[i] = True
+        for idx in range(len(L[value])):
+            if not visited[idx] and L[value][idx] == 1:
+                que.append([idx, distance[value] + 1])
+                visited[idx] = True
     return distance
-
-
-for _ in range(t):
+for _ in range(q):
     check, a, b = map(int, input().split())
-    if (check == 1):
+    if check == 1:
         L[a][b] = 1
         L[b][a] = 1
-    else:
+    else :
         L[a][b] = 0
         L[b][a] = 0
 
-    distance = bfs([1, 0])  # node, path
+    distance = bfs([1, 0])
 
-    for i in range(1, n + 1):
-        if (distance[i] == INF):
-            print(-1, end=" ")
+    for inx in range (1, n + 1):
+        if distance[inx] == INF:
+            print("-1", end=" ")
         else:
-            print(distance[i], end=" ")
+            print(distance[inx], end=" ")
     print()
