58abbf5fd0372241378f3732e48abe560d41b04a
박승찬 <seungchan141414@gmail.com>
2024-01-16 21:47:07 +0900

    https://www.acmicpc.net/problem/14502

--- pratice copy.py
+++ pratice copy.py
@@ -1,4 +1,4 @@
-
+# https://www.acmicpc.net/submit/14502
 
 """
 4 6
@@ -20,6 +20,9 @@ N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
 virus = []
 safe_zone = []
+dy = [0, 0, -1, 1]
+dx = [-1, 1, 0, 0]
+result = 0;
 
 for y in range(N):
     for x in range(M):
@@ -29,13 +32,26 @@ for y in range(N):
             virus.append([y, x])
 
 def bfs():
+    global result
     ch_virus = deque([])
-    cnt = len()
+    cnt = len(safe_zone) - 3
     for y, x in virus:
         ch_virus.append([y, x])
+    while ch_virus:
+        yy, xx = ch_virus.popleft()
+        for i in range(len(dy)):
+            ny = yy + dy[i]
+            nx = xx + dx[i]
+            if 0 <= ny < N and 0 <= nx < M:
+                if ch_board[ny][nx] == 0:
+                    cnt -= 1
+                    ch_board[ny][nx] = 2
+                    ch_virus.append([ny, nx])
+    result = max(result, cnt)
 
-for comb in combinations(virus, 3):
+for comb in combinations(safe_zone, 3):
     ch_board = copy.deepcopy(board)
     for y, x in comb:
         ch_board[y][x] = 1
     bfs()
+print(result)
