ca53f0d702d614ed5266d69e537dfef636caaf73
박승찬 <seungchan141414@gmail.com>
2024-02-01 12:55:27 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/17681

---
+++ programmers/arhorith/01_31_2020_[1차] 비밀지도 .py
@@ -0,0 +1,23 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/17681
+
+def tenFromtwo(arr):
+    result = []
+    for num in arr:
+        binaryNum = str(bin(num))
+        result.append(binaryNum)
+    return result
+
+
+def solution(n, arr1, arr2):
+    answer = []
+
+    result_1 = tenFromtwo(arr1)
+    result_2 = tenFromtwo(arr2)
+    result = []
+    for i in range(n):
+        binary = str(bin(arr1[i] | arr2[i])[2:])
+        binary = binary.rjust(n, '0')
+        binary = binary.replace("0", " ").replace("1", "#")
+        result.append(binary)
+    answer = result
+    return answer
