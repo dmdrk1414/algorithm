b4db82e833f79612dff5e283207258c8628c7816
박승찬 <seungchan141414@gmail.com>
2024-02-02 08:45:41 +0900

    몫 구하기

--- programmers/arhorith/02_01_1단계문제.py
+++ programmers/arhorith/02_01_1단계문제.py
@@ -3,3 +3,16 @@
 # 숫자 비교하기
 def solution_1(num1, num2):
     return 1 if num1 == num2 else -1
+
+
+# 몫 구하기
+def solution_1_1(num1, num2):
+    return int(num1 / num2)
+
+
+def solution_1_2(num1, num2):
+    return int(num1 // num2)
+
+
+def solution_1_3(num1, num2):
+    return divmod(num1, num2)[0]
